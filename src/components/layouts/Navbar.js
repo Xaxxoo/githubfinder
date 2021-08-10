f55bb581a0e2f915d569053a43bf5ab1a7213cf9
mac <bnahmad83@gmail.com>
2021-08-10 11:02:49 +0100

    font awesome fix

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './navbar.css';
 
+// import { FaGithub } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 class Navbar extends Component {
   render() {
     return (
         <div className='nav'>
             <nav>
-                <h1>Github Finder</h1>
+                <h1>
+                    <FontAwesomeIcon icon={faGithub} className='github' />
+                    {this.props.logo}
+                </h1>
                 <ul>
                     <li>Link 1</li>
                     <li>Link 2</li>
                     <li>Link 3</li>
-                    <li>Link 1</li>
                 </ul>
             </nav>
         </div>
