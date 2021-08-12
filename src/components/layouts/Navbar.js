bcc12523dd4b3080ea991f772123869e41567e56
mac <bnahmad83@gmail.com>
2021-08-12 23:40:19 +0100

    nothing

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -6,13 +6,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 class Navbar extends Component {
-  render() {
+    render() {
+      const icon = <FontAwesomeIcon icon={faGithub} className='github' />;
     return (
         <div className='nav'>
             <nav>
                 <h1>
-                    <FontAwesomeIcon icon={faGithub} className='github' />
-                    {this.props.logo}
+                    {icon} {this.props.logo}
                 </h1>
                 <ul>
                     <li>Home</li>
